1390ede0249c0c15fd386d59b4f360b953b7adfd
Blake <bpro0821@gmail.com>
2019-08-15 15:14:06 -0500

    update styling, install dependencies

--- async-redux/src/App.js
+++ async-redux/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       ASYNC REDUX &#8669; Concepts: Middleware
-      </header>
+       <h1 className="App-header">ASYNC REDUX &#8669; Indian Cities</h1>
+      <div className= "user-exp">
+       <div className= "boxes">
+         User Input
+       </div>
+       <div className= "boxes">
+         API Data
+       </div>
+      </div>
     </div>
   );
 }
